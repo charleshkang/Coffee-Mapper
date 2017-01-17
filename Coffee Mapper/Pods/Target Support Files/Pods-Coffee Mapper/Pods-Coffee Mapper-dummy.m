0cfcfafabad3a521c0c8881de35d9cc7842ea8ed
charles <charleshkang1@gmail.com>
2017-01-16 19:49:15 -0500

    unchecked pods from git repo

--- Coffee Mapper/Pods/Target Support Files/Pods-Coffee Mapper/Pods-Coffee Mapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Coffee_Mapper : NSObject
-@end
-@implementation PodsDummy_Pods_Coffee_Mapper
-@end
--- Coffee Mapper/Pods/Target Support Files/Pods-Coffee Mapper/Pods-Coffee Mapper-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Coffee_MapperVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Coffee_MapperVersionString[];
-
