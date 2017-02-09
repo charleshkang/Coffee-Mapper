dd762548c01a5e3faedac9397c79169023f4da57
Charles <ckangkitchen@gmail.com>
2017-02-09 01:57:05 -0500

    updated podfile, refactoring old firebase code

--- Coffee Mapper/Coffee Mapper/DataService.swift
+++ Coffee Mapper/Coffee Mapper/DataService.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2016 Charles Kang. All rights reserved.
 //
 
-import Firebase
 import Foundation
+import Firebase
+
 
 class DataService {
     
@@ -16,30 +17,30 @@ class DataService {
     static let venue = Venue()
     
     // Properties
-    let FB_BASE_REF = Firebase(url: "\(baseURL)")
-    let FB_USER_REF = Firebase(url: "\(baseURL)/users")
-    var FB_REVIEW_REF = Firebase(url: "\(baseURL)/reviews")
+    let FB_BASE_REF = FIRDatabase.database().referenceFromURL("\(baseURL)")
+    let FB_USER_REF = FIRDatabase.database().referenceFromURL("\(baseURL)/users")
+    var FB_REVIEW_REF = FIRDatabase.database().referenceFromURL("\(baseURL)/reviews")
     
-    var BASE_REF: Firebase {
+    var BASE_REF: FIRDatabaseReference {
         return FB_BASE_REF
     }
     
-    var USER_REF: Firebase {
+    var USER_REF: FIRDatabaseReference {
         return FB_USER_REF
     }
     
-    var CURRENT_USER_REF: Firebase {
+    var CURRENT_USER_REF: FIRDatabaseReference {
         let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as? String
-        let currentUser = Firebase(url: "\(BASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
+        let currentUser = FIRDatabase.database().referenceFromURL("\(BASE_REF)").child("users").child(userID!)
         return currentUser
     }
     
-    var REVIEW_REF: Firebase {
+    var REVIEW_REF: FIRDatabaseReference {
         return FB_REVIEW_REF
     }
     
     func createNewAccount(uid: String, user: Dictionary<String, String>) {
-        USER_REF.childByAppendingPath(uid).setValue(user)
+        USER_REF.child(uid).setValue(user)
     }
     
     func createNewReview(review: Dictionary<String, AnyObject>) {
@@ -48,6 +49,6 @@ class DataService {
     }
     
     func addReviewForCoffeeShop(shopId: String, review: AnyObject) {
-        FB_REVIEW_REF.childByAppendingPath(shopId).childByAutoId().setValue(review)
+        FB_REVIEW_REF.child(shopId).childByAutoId().setValue(review)
     }
 }
